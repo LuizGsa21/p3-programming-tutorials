7c4f6ab65ae3d0f09e5b510b25e65c4c0569644c
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-04-03 23:12:00 -0500

    Initial init (empty file structure)

---
+++ pg_config.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+apt-get -qqy update
+apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-pip
+pip install bleach
+pip install oauth2client
+pip install requests
+pip install httplib2
+pip install Flask-WTF
+pip install Flask-SQLAlchemy
+pip install beautifulsoup4
+pip install Flask-Login
+pip install Flask-OpenID
+su postgres -c 'createuser -dRS vagrant'
+su vagrant -c 'createdb'
