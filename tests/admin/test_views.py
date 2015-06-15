98e45276e32ac3bf1ef6f89abd0823ab616edb49
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-06-15 01:57:12 -0500

    Delete tests

--- tests/admin/test_views.py
+++
@@ -1,8 +0,0 @@
-from tests import BaseTestCase
-
-
-class AdminTestCase(BaseTestCase):
-
-    def test_dashboard(self):
-        r = self.client.get('/admin/')
-        self.assertEqual(r.status_code, 200, 'status code failed')
