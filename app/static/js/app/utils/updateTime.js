88d6ebed0bf81447e0d515ea03b02b839447c50b
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-08-03 16:36:37 -0500

    Fix momentjs `fromNow()` output

--- app/static/js/app/utils/updateTime.js
+++ app/static/js/app/utils/updateTime.js
@@ -5,9 +5,14 @@ define(['jquery', 'momentjs'], function ($, moment) {
 			$elements = $('[data-time]');
 
 		$elements.each(function (i) {
-			var $this = $(this), localTime;
-			// parse utc time to local time
-			localTime = moment.utc($this.data('time'));
+			var $this = $(this), utcTime, localTime;
+			// time format: 2015-08-03T14:59:38.803775+00:00
+			utcTime = $this.data('time');
+			// Convert to "YYYY-MM-DDTHH:mm:ss" so `moment` wont round up when
+			// calling `fromNow()` which would sometimes change its output
+			// from "a few seconds ago" to "in a few seconds"
+			utcTime = utcTime.split('.')[0];
+			localTime = moment.utc(utcTime);
 			this.innerHTML = moment(localTime).fromNow();
 		});
 	}
