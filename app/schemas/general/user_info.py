64df8372f1d3c8c29c0b0c48e072a69d6921fa50
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-08-05 17:15:35 -0500

    Add `isAdmin` attribute to UserInfoSchema

--- app/schemas/general/user_info.py
+++ app/schemas/general/user_info.py
@@ -1,14 +1,14 @@
 from flask import url_for
 from marshmallow import fields
-from flask_login import current_user
+from flask_login import current_user as c_user
 from ..model import UserSchema
 
 
 class UserInfoSchema(UserSchema):
-    isLoggedIn = fields.Function(lambda obj: current_user.is_authenticated())
-    isAdmin = fields.Function(lambda obj: current_user.is_admin())
+    isLoggedIn = fields.Function(lambda obj: obj.id == c_user.id and c_user.is_authenticated())
+    isAdmin = fields.Function(lambda obj: obj.isAdmin)
     avatar = fields.Function(lambda obj: url_for('static', filename='uploads/avatar/' + obj.avatar))
     dateJoined = fields.DateTime()
 
     class Meta:
-        fields = ('username', 'email', 'firstName', 'avatar', 'lastName', 'dateJoined', 'isLoggedIn', 'isAdmin')
+        fields = ('id', 'username', 'email', 'firstName', 'avatar', 'lastName', 'dateJoined', 'isAdmin', 'isLoggedIn')
