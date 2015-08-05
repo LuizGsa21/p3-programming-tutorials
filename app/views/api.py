9e2b695a178deddeb27fe2cde1b64f20555609df
Luiz Arantes Sa <LArantesSa@icloud.com>
2015-08-05 17:17:00 -0500

    Update api views to always wrap json with `result`

--- app/views/api.py
+++ app/views/api.py
@@ -1,34 +1,53 @@
-from flask import Blueprint, jsonify
-from app.schemas import article_serializer, CategorySchema
-from app.models import Article, Category
+from flask import Blueprint, jsonify, flash, request
+from flask_login import current_user
+from app.models import Article, Category, User
+from app.schemas import (
+    article_serializer, category_serializer,
+    user_info_serializer, user_serializer
+)
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 
 @api_bp.route('/articles/')
 @api_bp.route('/articles/all/')
-def articles_to_json():
+def get_articles():
     articles = Article.query.all()
-    if articles is None:
-        result = None
-    else:
-        result, error = article_serializer.dump(articles, many=True)
-    return jsonify(articles=result)
+    if articles:
+        articles = article_serializer.dump(articles, many=True).data
+    result = {
+        'articles': articles
+    }
+    return jsonify(result=result)
 
 
 @api_bp.route('/articles/<int:id>/')
-def article_to_json(id):
+def get_article_by_id(id):
     article = Article.query.get(id)
-    if article is None:
-        result = None
-    else:
-        result, error = article_serializer.dump(article)
-    return jsonify(article=result)
+    if article:
+        article = article_serializer.dump(article).data
+    result = {
+        'article': article
+    }
+    return jsonify(result=result)
+
+@api_bp.route('/articles/author/<int:id>/')
+def get_articles_by_author_id(id):
+    articles = Article.query.filter_by(authorId=id)
+    if articles:
+        articles = article_serializer.dump(articles, many=True).data
+    result = {
+        'articles': articles
+    }
+    return jsonify(result=result)
 
 
 @api_bp.route('/categories/')
-def categories_to_json():
+def get_categories():
     categories = Category.query.all()
-    serializer = CategorySchema(many=True)
-    result, error = serializer.dump(categories)
-    return jsonify(categories=result)
+    if categories:
+        categories = category_serializer.dump(categories).data
+    result = {
+        'categories': categories
+    }
+    return jsonify(result=result)
